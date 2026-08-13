9ce6fe2ddfc089a6bde7b93d9a22b60cfa9a9b27
Matheus Santos Volotão <math.volotao@gmail.com>
2026-08-13 15:39:32 -0300

    aula de tipos primitivos

---
+++ DevQuest2.0/6-fundamentos-js/2-tipos-primitivos/index.js
@@ -0,0 +1,17 @@
+// === tipos primitivos  ==//
+
+// string
+let nome = "Alice";
+
+// number
+let idade = 25;
+    
+
+//boolean 
+let isStudent = true;
+
+// undefined - nao foi iniciado com algum valor
+let undefinedValue;
+
+// null - nao tem valor definido
+let nullValue = null;
