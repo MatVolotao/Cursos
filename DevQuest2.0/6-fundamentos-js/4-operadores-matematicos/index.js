cefc289b3b25fe890ce0642f0c452a700ac6f1b2
Matheus Santos Volotão <math.volotao@gmail.com>
2026-08-13 20:27:31 -0300

    Adiciona conteudo de operadores matematicos

---
+++ DevQuest2.0/6-fundamentos-js/4-operadores-matematicos/index.js
@@ -0,0 +1,33 @@
+// operadores matematicos
+
+///soma
+const numero1 = 3
+const numero2 = 5
+const stringnumero = "10"
+const resultadoSoma = numero1 + numero2
+const resultadoSomaString = numero1 + stringnumero    
+console.log(resultadoSoma)
+console.log(resultadoSomaString)
+// agora vamos printar os tipos de dados das variaveis
+console.log(typeof resultadoSoma)
+console.log(typeof resultadoSomaString)
+
+//subtração
+const resultadoSubtracao = numero1 - numero2
+console.log(resultadoSubtracao)
+console.log(typeof resultadoSubtracao)
+
+//multiplicação
+const resultadoMultiplicacao = numero1 * numero2
+console.log(resultadoMultiplicacao)
+console.log(typeof resultadoMultiplicacao)
+
+//divisão
+const resultadoDivisao = numero1 / numero2
+console.log(resultadoDivisao)
+console.log(typeof resultadoDivisao)
+
+//resto
+const resultadoResto = numero1 % numero2
+console.log(resultadoResto)
+console.log(typeof resultadoResto)
