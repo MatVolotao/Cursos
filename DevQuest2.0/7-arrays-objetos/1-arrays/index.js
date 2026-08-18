d34f201ab32921be7dc8bbb666119af9c1ecc21c
Matheus Santos Volotão <math.volotao@gmail.com>
2026-08-18 18:06:45 -0300

    adicionado conteudo sobre arrays

---
+++ DevQuest2.0/7-arrays-objetos/1-arrays/index.js
@@ -0,0 +1,15 @@
+// == arrays == //
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+// acessando o primeiro elemento do array
+console.log(array[0]);
+
+//arras dentro de arrays
+
+const avaliacoesFilmes = [
+    ["Homen-Aranha", 8.5],
+    ["Batman", 9.0],
+    ["Superman", 8.0]
+]
+
+console.log(avaliacoesFilmes[0][0]); // acessando o nome do primeiro filme
