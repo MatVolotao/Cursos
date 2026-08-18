f5e7d9765772e0bbd72d11960153aafc3e180261
Matheus Santos Volotão <math.volotao@gmail.com>
2026-08-18 16:30:12 -0300

    funções anonimas e arrow functions

--- DevQuest2.0/6-fundamentos-js/7-funcoes/index.js
+++ DevQuest2.0/6-fundamentos-js/7-funcoes/index.js
@@ -1,6 +1,7 @@
 //funções nomeadas
 // O que são?
 // Funções nomeadas são blocos de código reutilizáveis que podem ser chamados pelo seu nome. Elas permitem organizar o código, evitar repetição e facilitar a manutenção.
+// tem hosting, ou seja, podem ser chamadas antes de serem declaradas.
 function saudacao() {
 	console.log("Olá, seja bem-vindo(a)!");
 }
@@ -17,3 +18,23 @@ function mensagemPersonalizada(nome) {
 }
 
 console.log(mensagemPersonalizada("Maria"));
+
+
+//funções anonimas
+// não tem hosting, ou seja, não podem ser chamadas antes de serem declaradas.
+const saudacaoAnonima = function() {
+    console.log("Olá, seja bem-vindo(a)!");
+}
+
+saudacaoAnonima();
+//quando usar 
+// Funções anônimas são úteis quando você precisa de uma função temporária ou quando a função é usada apenas uma vez. Elas também são frequentemente usadas em callbacks e em situações onde a função precisa ser passada como argumento para outra função.
+
+
+
+// arrow functions
+const saudacaoArrow = () => {
+    console.log("Olá, seja bem-vindo(a)!");
+}
+//quando usar
+// Arrow functions são úteis para funções curtas e simples, especialmente quando você precisa de uma função como argumento para outra função. Elas também são frequentemente usadas em métodos de array, como map, filter e reduce. Além disso, arrow functions não possuem seu próprio contexto de "this", o que pode ser vantajoso em certos casos.
